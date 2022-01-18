a520d07665fcae7d918be28e29eb858ad0015477
Mohammed Chakmakchi <mohammed.chakmakchi@lego.com>
2022-01-18 17:20:34 +0100

    quick fix

--- javascript/index.js
+++ javascript/index.js
@@ -385,5 +385,5 @@ function checkIfNewHighscore() {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] != 0) list[i].innerHTML = arr[i];
   }
-  listParent.style = "display: block";
+  if (arr[0] != 0) listParent.style = "display: block";
 }
